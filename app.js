03c2057caefda1569248f05f5be1daef60ee8909
Mohammad Hussain <165736304+MohammadHussain5@users.noreply.github.com>
2024-06-24 15:03:58 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+import inquirer from 'inquirer';
+async function main() {
+    const input = await inquirer.prompt([
+        {
+            name: "name1",
+            type: "input",
+            message: "Write the sentence for count the words:"
+        }
+    ]);
+    // Trim and split the input to count words
+    const counting = input.name1.trim().split(" ");
+    console.log(counting);
+    console.log(`Total words: ${counting.length}`);
+}
+main();
